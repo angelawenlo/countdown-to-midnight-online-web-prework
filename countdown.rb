6b9ada53bf5ea15e1569659a9e1b42a0f154c8ed
Angela Lo <angelawenlo@gmail.com>
2019-05-15 13:36:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,20 +1,18 @@
 #write your code here
 
 def countdown(number)
-  counter=0
-  while counter<number
+  while number>0
     puts "#{number} SECOND(S)!"
-    counter-=1
+    number-=1
   end
-    puts "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  counter=0
-  while counter<number
+  while number >0
     puts "#{number} SECOND(S)!"
-    counter-=1
-    sleep(5)
+    sleep(1)
+    number-=1
   end
-    puts "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
 end
